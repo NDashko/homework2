0f1dba65724e6d8b8ba0714c002307d6912a7708
Наталья Дашко <ndashko@mail.ru>
2023-09-30 11:33:56 +0300

    Добавлена задача 2

---
+++ task2/Main.java
@@ -0,0 +1,17 @@
+package task2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] intArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }; // Не обьявлен массив
+
+        try {
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+
+    }
+}
